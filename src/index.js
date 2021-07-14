afd99a78ff7a6800e38b008026eb41780cac06c4
aslam <cntctme.aslam@gmail.com>
2021-07-14 21:26:16 +0530

    topbar added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Routes from "routes";
+import Topbar from "components/Topbar";
+
+const App = () => {
+  return (
+    <Router>
+      <Topbar />
+      <Routes />
+    </Router>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
